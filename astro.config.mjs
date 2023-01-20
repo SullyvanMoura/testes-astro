0d307c93a6af783ea09d3ab9953b1ae3224e20f2
sullyvanmoura <sullyvanmoura@hotmail.com>
2023-01-20 18:22:30 -0300

    feat: deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://sullyvanmoura.github.io',
+    base: '/testes-astro',
+});
