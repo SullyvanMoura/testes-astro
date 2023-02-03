9420a78a5baa78c1549d41cacfc47a179e14b460
SullyvanMoura <sullyvanmoura@hotmail.com>
2023-02-03 16:16:07 -0300

    feat: adicao do tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-    site: 'https://sullyvanmoura.github.io',
-    base: '/testes-astro',
+  site: 'https://sullyvanmoura.github.io',
+  base: '/testes-astro',
+  integrations: [tailwind()]
 });
